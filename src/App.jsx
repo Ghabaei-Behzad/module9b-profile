e045f17078cccdc58fee337b417384ac61061c3f
Behzad Ghabaei <Ghabaei_Behzad@student.smc.edu>
2026-02-03 19:31:23 -0800

    Updated porfile with emojis and another character.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,31 @@
 
+import UserProfile from './UserProfile';
+
+function App() {
+  return (
+    <div style={{ padding: '40px', backgroundColor: '#f0f2f5', minHeight: '100vh' }}>
+      <UserProfile 
+        name="Jane Smith" 
+        bio="Creative Designer based in NYC." 
+        image="https://imgproxy.attic.sh/insecure/f:webp/q:90/w:384/plain/https://attic.sh/8hjgvpcrgz7ol3kng78ipde1nt4u"
+      />
+      {/* Bonus: Second component using default props */}
+      <UserProfile /> 
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+////////////////////////////////////////////////////////////
+/*
 // src/App.jsx
 import UserProfile from './UserProfile';
 
@@ -12,9 +39,9 @@ function App() {
 
 export default App;
 
+*/
 
-
-
+///////////////////////////////////////////////////////////////////
 /*
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
